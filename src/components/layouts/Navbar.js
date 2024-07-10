7f239bccc3f7f44e4a3d03e8a412bc3294dc0044
Md Alam Rahman <98524939+mdalamrahman4@users.noreply.github.com>
2024-07-10 22:26:50 +0530

    changes for smaller screens

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   }, []);
   if(!mounted) return null;
   return (
-    <header className="text-white-100 sticky top-0 z-50 bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 body-font">
+    <header className="text-white-100 lg:sticky lg:top-0 lg:z-50 bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 body-font">
       <div className="container mx-auto flex flex-wrap  p-3 flex-col md:flex-row items-center">
         <Link href={"/"} className="px-8 md:px-4 flex title-font font-extrabold items-center  uppercase text-gray-100">
           <Image alt="pizzeria" src="/img1.png" width={90} height={70} />
